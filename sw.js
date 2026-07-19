2f648130913af6946afa8f4aab9517ba9a42c91a
seanques-gif <desucatan.sean@gmail.com>
2026-07-19 16:44:28 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'orga-naes-v1';
+const ASSETS = [
+  './Orga-naes.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.url.includes('firebase') || e.request.url.includes('googleapis')) return;
+  e.respondWith(
+    fetch(e.request).then(resp => {
+      const clone = resp.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      return resp;
+    }).catch(() => caches.match(e.request))
+  );
+});
