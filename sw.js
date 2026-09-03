3345e94b0092df05fcbadf10bd042a5240619f40
seanques-gif <desucatan.sean@gmail.com>
2026-09-03 20:12:58 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE_VERSION = '2026-09-03-0001';
+const CACHE_VERSION = '2026-09-03-0002';
 const CACHE_NAME = 'orga-naes-' + CACHE_VERSION;
 const ASSETS = [
-  './Orga-naes.html',
+  './index.html',
   './styles.css',
   './app.js',
   './pwa.js',
