bf6dc1786867a040b75decb29066aa37e85cdb1e
Sean Ques <306709851+seanques-gif@users.noreply.github.com>
2026-09-03 20:52:11 +0800

    Change cache version and update assets in sw.js
    Updated cache version and modified asset list.

--- sw.js
+++ sw.js
@@ -1,11 +1,7 @@
-const CACHE_VERSION = '2026-09-03-0002';
+const CACHE_VERSION = '2026-09-02-0001';
 const CACHE_NAME = 'orga-naes-' + CACHE_VERSION;
 const ASSETS = [
-  './index.html',
-  './styles.css',
-  './app.js',
-  './pwa.js',
-  './firebase-sync.js',
+  './Orga-naes.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
